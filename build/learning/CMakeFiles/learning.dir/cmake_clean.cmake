f9455aaee04a4f67e921c2c7cc71fda164b916f1
root <iamcoderx@gmail.com>
2021-09-09 05:56:02 +0000

    first

---
+++ build/learning/CMakeFiles/learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning.dir/learning.obj"
+  "learning.pdb"
+  "learning.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
